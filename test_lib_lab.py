75e1c99cbb9e21a7641384f9f5598e2ad4a6fb2b
Artur <arturbelaykov@gmail.com>
2024-01-17 02:24:46 +0300

    Поправил clr и изменил тесты на норм

--- test_lib_lab.py
+++ test_lib_lab.py
@@ -1,30 +1,12 @@
+import unittest
 from lib_lab import *
 
 
-def test(fun, inp, out):
-	z = []
-	for i, q in zip(inp, out):
-		a = fun(i)
-		if a == q:
-			z.append(False)
-		else:
-			z.append((True, a))
-	if not all(z):
-		print("clr: OK")
-	else:
-		for i in z:
-			print(f"clr: X == `{i[1]}`: {type(i[1])}")
-
-
-test(clr, [
-	"  12,3 21   4 ",
-	"    123,25  125 6,47 ",
-	"124, 1246  ,  46 ",
-	"1.24, 12.46  ,  46 ",
-], [
-	"12.3 21 4",
-	"123.25 125 6.47",
-	"124 1246 46 ",
-	"1.24 12.46 46",
-])
-
+class Testtest_lib_lab(unittest.TestCase):
+	def test_clr_sp(self):
+		self.assertEqual(clr_sp("  12,3 21   4 "), "12,3 21 4")
+		self.assertEqual(clr_sp("    123,25  125 6,47 "), "123,25 125 6,47")
+		self.assertEqual(clr_sp("124, 1246  ,  46 "), "124,1246,46")
+		self.assertEqual(clr_sp("1.24, 12.46  ,  46 "), "1.24,12.46,46")
+		with self.assertRaises(TypeError):
+			clr_sp(1234)
