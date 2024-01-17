268b2a3a4c18bd871482b5598e8e9f1b1613cc44
Artur <arturbelaykov@gmail.com>
2024-01-17 03:14:16 +0300

    Изменил stu, переместил T и изменил его, добавил тесты

--- test_lib_lab.py
+++ test_lib_lab.py
@@ -10,3 +10,14 @@ class Testtest_lib_lab(unittest.TestCase):
 		self.assertEqual(clr_sp("1.24, 12.46  ,  46 "), "1.24,12.46,46")
 		with self.assertRaises(TypeError):
 			clr_sp(1234)
+
+	def test_stu(self):
+		self.assertEqual(stu(4), 2.776)
+		self.assertEqual(stu(4, 2), 2.776)
+		self.assertEqual(stu(4, 3), 4.604)
+		with self.assertRaises(ValueError) as err:
+			stu(9999, 4)
+		self.assertEqual(err.exception.args[0], "Нету в таблице такой доверительной вероятности как 4")
+		with self.assertRaises(ValueError) as err:
+			stu(9998, 3)
+		self.assertEqual(err.exception.args[0], "Нету в таблице такой степени свободы как 9998")
