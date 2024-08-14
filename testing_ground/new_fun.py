7342d5d30a192772f883586f7e9bd0f67814dfac
Artur <arturbelaykov@gmail.com>
2024-08-15 02:48:52 +0300

    Набросок для нового функционала 4.

--- testing_ground/new_fun.py
+++ testing_ground/new_fun.py
@@ -3,18 +3,25 @@ from kivymd.uix.screen import MDScreen
 from kivymd.uix.screenmanager import MDScreenManager
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivy.properties import ListProperty, StringProperty, BooleanProperty, NumericProperty, ObjectProperty
+from kivy.uix.button import Button
 
 
 class Box(MDBoxLayout):
     def get_data(self):
         if self.children:
-            for i in self.children:
-                print(i.text)
+            return [i.text for i in self.children]
 
 
 class ScreenStart(MDScreen):
     container_box = ObjectProperty()
     title_name = StringProperty()
+
+    def get_data(self):
+        data = []
+        for i in self.container_box.children:
+            if type(i) is Box:
+                data.append(i.get_data())
+        return data
 
 
 class RootWidget(MDScreenManager):
@@ -22,15 +29,19 @@ class RootWidget(MDScreenManager):
 
 
 class NewFun(MDApp):
-    # def __init__(self, **kvargs):
-    #     super().__init__(**kvargs)
-    #     self.root_widget = RootWidget()
-    #     self.screen_start = ScreenStart(title_name="AppBar small")
-    #     self.root_widget.add_widget(self.screen_start)
+    def __init__(self, **kvargs):
+        super().__init__(**kvargs)
+        self.root_widget = RootWidget()
+        self.screen_start = ScreenStart(title_name="AppBar small")
+        # self.root_widget.add_widget(self.screen_start)
 
     def build(self):
         self.theme_cls.theme_style = "Dark"
-        # return self.root_widget
+        m = MDScreenManager()
+        s = MDScreen()
+        s.add_widget(Button())
+        m.add_widget(s)
+        return m
 
 
 if __name__ == '__main__':
