63d4c27abf4cdc8a7d99c1a9558620a65df83819
Artur <arturbelaykov@gmail.com>
2024-08-15 02:33:13 +0300

    Набросок для нового функционала 3.

--- testing_ground/new_fun.py
+++ testing_ground/new_fun.py
@@ -1,11 +1,37 @@
 from kivymd.app import MDApp
-from kivymd.uix.filemanager.filemanager import MDFileManager
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.screenmanager import MDScreenManager
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivy.properties import ListProperty, StringProperty, BooleanProperty, NumericProperty, ObjectProperty
 
 
-class NewFunf(MDApp):
+class Box(MDBoxLayout):
+    def get_data(self):
+        if self.children:
+            for i in self.children:
+                print(i.text)
+
+
+class ScreenStart(MDScreen):
+    container_box = ObjectProperty()
+    title_name = StringProperty()
+
+
+class RootWidget(MDScreenManager):
+    pass
+
+
+class NewFun(MDApp):
+    # def __init__(self, **kvargs):
+    #     super().__init__(**kvargs)
+    #     self.root_widget = RootWidget()
+    #     self.screen_start = ScreenStart(title_name="AppBar small")
+    #     self.root_widget.add_widget(self.screen_start)
+
     def build(self):
         self.theme_cls.theme_style = "Dark"
+        # return self.root_widget
 
 
 if __name__ == '__main__':
-    NewFunf().run()
+    NewFun().run()
